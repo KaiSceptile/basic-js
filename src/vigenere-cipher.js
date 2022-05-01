6a5326173a8d46ad0254590e05cf1c75e2f26de7
KaiSceptile <90106146+KaiSceptile@users.noreply.github.com>
2022-05-01 23:39:49 +0300

    feat: update

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,15 +19,54 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
-class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+class VigenereCipheringMachine{
+    
+    constructor(method){
+        this.method=method;
+        this.alphabet="abcdefghijklmnopqrstuvwxyz";
+    }
+    getfullkey(str,key){
+        let result="";
+        let j=0;
+        for (let i=0;i<str.length;i++){
+            if (this.alphabet.indexOf(str[i])==-1) {result=result+str[i];} else
+            {result=result+key[j % key.length]; j++;}
+        }
+        return result;
+    }
+    reverse(str){
+        let result="";
+        for (let i=str.length-1;i>=0;i--){
+            result=result+str[i];
+        }
+        return result;
+    }
+    encrypt(str,key){
+       
+        str=str.toLowerCase();
+        let result="";
+        let fullkey=this.getfullkey(str,key);
+        for (let i=0;i<str.length;i++){
+            let index=this.alphabet.indexOf(str[i])+this.alphabet.indexOf(fullkey[i]);
+            if ((this.alphabet.indexOf(str[i])==-1)||(this.alphabet.indexOf(fullkey[i])==-1)) {
+                result=result+str[i];} else {result=result+this.alphabet[index % this.alphabet.length];}
+        }
+        if (this.method==false) return this.reverse(result.toUpperCase());
+        return result.toUpperCase();
+    }
+    decrypt(str,key){
+        
+        str=str.toLowerCase();
+        let result="";
+        let fullkey=this.getfullkey(str,key);
+        for (let i=0;i<str.length;i++){
+            let index=this.alphabet.indexOf(str[i])-this.alphabet.indexOf(fullkey[i]);
+            if ((this.alphabet.indexOf(str[i])==-1)||(this.alphabet.indexOf(fullkey[i])==-1)) {
+                result=result+str[i];} else {result=result+this.alphabet[(this.alphabet.length+index) % this.alphabet.length];}
+        }
+        if (this.method==false) return this.reverse(result.toUpperCase());
+        return result.toUpperCase();
+    }
 }
 
 module.exports = {
