c2120389503ed5728be092d75bf1bd04ac92c908
KaiSceptile <90106146+KaiSceptile@users.noreply.github.com>
2022-05-01 23:37:48 +0300

    feat: update

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -4,28 +4,44 @@ const { NotImplementedError } = require('../extensions/index.js');
  * Implement chainMaker object according to task description
  * 
  */
-const chainMaker = {
-  getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-};
+const chainMaker= {
+    chainArray: [],
+    getLength: function(){
+        return this.chainArray.length;
+    },
+
+    removeLink(position){
+        if (!(typeof position =='number')||(this.chainArray.length<position)) {this.chainArray=[]; throw Error("You can't remove incorrect link!")}
+        this.chainArray=this.chainArray.slice(position);
+        return this;
+    },
+
+    addLink: function(value){
+        if (arguments.length==0) {this.chainArray.push('')} else {this.chainArray.push(value)}
+        return this;
+    },
+    
+    
+    reverseChain(){
+        for (let i=0; i<this.chainArray.length/2; i++){
+            let swap=this.chainArray[i];
+            this.chainArray[i]=this.chainArray[this.chainArray.length-1-i];
+            this.chainArray[this.chainArray.length-1-i]=swap;
+        }
+        return this;
+    },
+
+    finishChain(){
+        let res='';
+        for (let i of this.chainArray){
+            res+='( '+i+' )~~'
+        }
+        if (res.length>0) {res=res.slice(0,-2)}
+        this.chainArray=[];
+        return res;
+    }
+    }
+
 
 module.exports = {
   chainMaker
